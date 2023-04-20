3dda95e95c82ffc37bcee910a75fd14703227edd
t-ishino <89590754+Lachryma7@users.noreply.github.com>
2023-04-20 15:14:44 +0900

    #8 use Lib and LibLib2

--- R/arith.R
+++ R/arith.R
@@ -7,4 +7,7 @@ minus <- function(a, b)
 areaOfCircle <- function(r)
     return(3 * square(r))
 
+areaOfTriangle <- function(bottom, height)
+    return(divide(multiply(bottom, height), 2))
+
 # aaaaaaa
--- tests/testthat/test-arith.R
+++ tests/testthat/test-arith.R
@@ -12,3 +12,8 @@ test_that("areaOfCircle", {
     actual <- areaOfCircle(2)
     expect_identical(actual, 12)
 })
+
+test_that("areaOfTriangle", {
+    actual <- areaOfTriangle(2,3)
+    expect_identical(actual, 3)
+})
